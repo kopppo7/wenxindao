8f44115ca07441fddbfbce9a642bf3a004c43b77
hesiyuan <315765993@qq.com>
2023-01-03 22:23:34 +0800

    隐私设置 图片

--- 04zhutipaidui/setHouse/setHouse.js
+++ 04zhutipaidui/setHouse/setHouse.js
@@ -1,5 +1,6 @@
 // 04zhutipaidui/setHouse/setHouse.js
 import {
+    findByAskPartyOne,
     updateImMsg,
 } from "../api";
 import config from "../../utils/config";
@@ -11,7 +12,8 @@ Page({
     data: {
         useMes: {},
         isShow: true,
-        themeId:''
+        themeId:'',
+        detailObj:{}
     },
     //上传本地图片
     radioChange(e) {
@@ -91,6 +93,28 @@ Page({
             })
         }
     },
+
+    getRoomDet:function () {
+        findByAskPartyOne({ id: this.data.themeId }).then(res => {
+            let obj = res.data.data;
+            obj.tips = obj.tips.split(' ');
+            console.log(obj);
+            let richText1 = obj.detailsText
+            let richText2 = obj.flowText
+            obj.detailsText = richText1
+                .replace(/\<img/gi, '<img style="width:100%;height:auto;"')
+                .replace(/\<p/gi, '<p class="p_class"')
+                .replace(/\<span/gi, '<span class="span_class"')
+            obj.flowText = richText2
+                .replace(/\<img/gi, '<img style="width:100%;height:auto;"')
+                .replace(/\<p/gi, '<p class="p_class"')
+                .replace(/\<span/gi, '<span class="span_class"')
+            this.setData({
+                detailObj: obj
+            })
+        })
+    },
+
     /**
      * 生命周期函数--监听页面加载
      */
@@ -104,6 +128,7 @@ Page({
         this.setData({
             themeId: options.id,
         })
+        this.getRoomDet()
     },
 
     /**
