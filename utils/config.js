2aa76ad6f98d144b64816dc2d815655225bd718b
wangtingting <1664979201@qq.com>
2023-01-16 21:46:35 +0800

    修改

--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,5 @@
 // 如果有其它配置项则在下面新增并 export
-const DOMAIN ='https://www.wxdao.net/user';//'http://192.168.0.100:8003';//
+const DOMAIN ='https://www.wxdao.net/user';//'http://192.168.1.102:8003';//
 const ORDER = 'https://www.wxdao.net/order';
 export default {
     getDomain: DOMAIN,
