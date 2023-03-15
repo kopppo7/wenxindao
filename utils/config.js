724db15ea99563e3e99afa7f3e5683a235deaaf5
renpengjing <849592269@qq.com>
2023-03-15 21:33:39 +0800

    域名修改提交

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,6 @@
 // 如果有其它配置项则在下面新增并 export
-const DOMAIN = 'https://wenxin.duomixiong.com/user'//'http://192.168.0.104:8003';//
-const ORDER = 'https://wenxin.duomixiong.com/order';
+const DOMAIN = 'https://wenxin.wxdao.net/user'//'http://192.168.0.104:8003';//
+const ORDER = 'https://wenxin.wxdao.net/order';
 export default {
     getDomain: DOMAIN,
     getOrder: ORDER
